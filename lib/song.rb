55631c787a7dd08819f742b9f485eef4a871f74b
Greg Morgan <gregm9999@gmail.com>
2018-11-09 20:37:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count=0
+  @@genres=[]
 
   def initialize
     @@count+=1
@@ -10,4 +11,21 @@ class Song
   def count
     @@count
   end
+
+  def genres
+
+  end
+
+  def artists
+
+  end
+
+  def genre_count
+
+  end
+
+  def artist_count
+
+  end
+
 end
